441ef574e93caf68b5fd13faaed9c45b33238474
VerzilinSeva <144003819+VerzilinSeva@users.noreply.github.com>
2023-10-09 11:44:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+window.addEventListener("DOMContentLoaded", function (event){});
 
+function calculate() {
+    let number = Number(document.getElementById("Toys_Count").value);
+    let price = Number(document.getElementById("Toys_Price").value);
+
+
+    let exp = new RegExp(/^[0-9]+$/i);
+    if (
+        !(
+            exp.test(number) && exp.test(price)
+        )
+    ) {
+        alert("You should input positive integers");
+    } else if (number == "") {
+        alert("You are not input number of toys");
+    } else if (price == "") {
+        alert("You are not input price of toys");
+    } else {
+        const cost = number * price;
+        document.getElementById("cost").innerHTML = "Final price: " + cost;
+    }
+}
