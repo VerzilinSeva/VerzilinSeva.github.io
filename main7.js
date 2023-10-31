e7ec725fbc486f881a6c9e44313ae832899400ed
VerzilinSeva <144003819+VerzilinSeva@users.noreply.github.com>
2023-10-31 16:39:11 +0300

    Update main7.js

--- main7.js
+++ main7.js
@@ -1,34 +1,18 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("item");
-    var dots = document.getElementsByClassName("slider-dots_item");
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+$(document).ready(function(){
+    $(".gallery").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+              breakpoint: 850,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+        
+      });
+  });
