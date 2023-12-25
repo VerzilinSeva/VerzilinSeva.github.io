1bb7a16a6db306dc75ea975a3cc785c09ba1dc7d
VerzilinSeva <144003819+VerzilinSeva@users.noreply.github.com>
2023-12-25 11:28:52 +0300

    Create style6.js

---
+++ style6.js
@@ -0,0 +1,60 @@
+.container {
+    width: 50%;
+    background-color:lightseagreen;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    border-width: 2px;
+    border-style: solid;
+    border-color: steelblue;
+    border-radius: 10px;
+    padding: 10px;
+    padding-bottom: 20px;
+}
+
+body {
+     display: flex;
+     flex-direction: column;
+     justify-content: center;
+     align-items: center;
+     font-family: cursive;
+ }
+
+button {
+    width: 25%;
+    margin: 10px;
+}
+
+input {
+    margin: 10px;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+}
+
+h2 {
+    margin-bottom: 0;
+}
+
+.form2 {
+     width: 50%;
+     background-color:lightseagreen;
+     margin: 20px;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     flex-direction: column;
+     border-width: 2px;
+     border-style: solid;
+     border-color: steelblue;
+     border-radius: 10px;
+     padding: 10px;
+ }
+
+#result2 {
+     margin: 10px;
+ }
